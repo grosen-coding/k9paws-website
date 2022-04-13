bd1d6a13ac09df1b56088ceb5781dcd5543714f3
k9paws <candogservices@gmail.com>
2022-04-13 19:26:05 -0400

    created note Modal, not yet functional

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,5 +1,7 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {toast} from 'react-toastify'
+import Modal from 'react-modal'
+import {FaPlus} from 'react-icons/fa'
 import  {useSelector, useDispatch} from 'react-redux'
 import {getReport, closeReport } from '../features/reports/reportSlice';
 import {getNotes, reset as notesReset} from '../features/notes/noteSlice'
@@ -8,7 +10,26 @@ import BackButton from '../components/BackButton'
 import Loading from '../components/Loading' 
 import NoteItem from '../components/NoteItem' 
 
+// Modal Styles
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative'
+  }
+}
+
+Modal.setAppElement('#root')
+
 function Report() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [noteText, setNoteText] = useState("")
+
   const {report, isLoading, isSuccess, isError, message} = useSelector((state) => state.reports)
 
   const {notes, isLoading: notesIsLoading} = useSelector((state) => state.notes)
@@ -36,6 +57,19 @@ const onReportClose = () =>
    toast.success('Report Successfully Closed')
    navigate('/reports')
  }
+
+//  Create note submit
+const onNoteSubmit = (e) => {
+  e.preventDefault()
+  console.log('Submit')
+  closeModal()
+}
+
+// Open/Close Modal
+const openModal = () => setModalIsOpen(true)
+const closeModal = () => setModalIsOpen(false)
+
+
   if(isLoading || notesIsLoading) {
     return <Loading />
   }
@@ -63,6 +97,27 @@ const onReportClose = () =>
           </div>
           <h2>Notes</h2>
       </header>
+
+      {report.status !== 'closed' && (
+        <button onClick={openModal} className="btn"><FaPlus /> Add Note</button>
+      )}
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel="Add Note">
+
+        <h2>Add Note</h2>
+        <button className="btn-close" onClick={closeModal}>X</button>
+        
+        <form onSubmit={onNoteSubmit}>
+        <div className="form-group">
+          <textarea name="noteText" id="noteText" className='form-control' placeholder='Note text' value={noteText} onChange={(e) => setNoteText(e.target.value)}></textarea>
+        </div>
+        <div className="form-group">
+          <button className="btn" type='submit'>Submit</button>
+        </div>
+
+        </form>
+
+      </Modal>
 
       {notes.map((note) => (
         <NoteItem key={note._id} note={note} />
