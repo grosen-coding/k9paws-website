f0cf2ea3e82dc41e0c61596cb1458d3cd2b29346
k9paws <candogservices@gmail.com>
2022-04-13 18:52:53 -0400

    created noteService

--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -3,10 +3,31 @@ import noteService from "./noteService";
 
 const initialState = {
   notes: [],
+  isError: false,
   isSuccess: false,
   isLoading: false,
   message: "",
 };
+
+// Get Client Notes
+export const getNotes = createAsyncThunk(
+  "notes/getAll",
+  async (reportId, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.client.token;
+      return await noteService.getNotes(reportId, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const noteSlice = createSlice({
   name: "note",
