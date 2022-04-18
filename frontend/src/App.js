1c157d00f8ddb7de4f687d7a89742ffc84353ae8
k9paws <candogservices@gmail.com>
2022-04-17 21:46:05 -0400

    finialized setting up sass structure, linking files, removing compile errors, linking existing basic styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <>
       <Router>
-        <div className="container">
+        <div className="wrapper">
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
