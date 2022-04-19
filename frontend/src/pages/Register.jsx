f2fb93cd2cf7017c1e11dc277d10193e6805ac61
k9paws <candogservices@gmail.com>
2022-04-19 00:25:51 -0400

    updated register styling

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -67,65 +67,67 @@ function Register() {
 
     return (
 
-        <>
-            <section className="heading">
-                <h1>
-                    <FaUser /> Register
-                </h1>
-                <p>Please create an account</p>
-            </section>
+        <section className='wrapper'>
+            <div className="register-container">
+                <div className="register-heading">
+                    <h1>
+                        <FaUser /> New Client Register
+                    </h1>
+                    <p>For new and active <strong>K<span className='logo--nine-sm'>9</span>PAWS</strong> clients, you may have been requested to complete <strong>Progress Training Reports</strong>. If so, please begin by creating an account below.</p>
+                </div>
 
-            <section className="form">
-                <form onSubmit={onSubmit}>
-                    <div className="form-group">
-                        <input 
-                        type="text" 
-                        className="form-control" 
-                        id='name' name="name" 
-                        value={name} 
-                        onChange={onChange} 
-                        placeholder="Enter your name" 
-                        required />
-                    </div>
-                    <div className="form-group">
-                        <input 
-                        type="email" 
-                        className="form-control" 
-                        id='email' name="email" 
-                        value={email} 
-                        onChange={onChange} 
-                        placeholder="Enter your email address" 
-                        required />
-                    </div>
-                    <div className="form-group">
-                        <input 
-                        type="password" 
-                        className="form-control" 
-                        id='password' 
-                        name="password" 
-                        value={password} 
-                        onChange={onChange} 
-                        placeholder="Enter a password" 
-                        required />
-                    </div>
-                    <div className="form-group">
-                        <input 
-                        type="password" 
-                        className="form-control" 
-                        id='password2' 
-                        name="password2" 
-                        value={password2} 
-                        onChange={onChange} 
-                        placeholder="Confirm your password" 
-                        required />
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-block">Submit</button>
-                    </div>
+                <div className="form">
+                    <form onSubmit={onSubmit}>
+                        <div className="form-group">
+                            <input 
+                            type="text" 
+                            className="form-control" 
+                            id='name' name="name" 
+                            value={name} 
+                            onChange={onChange} 
+                            placeholder="Enter your name" 
+                            required />
+                        </div>
+                        <div className="form-group">
+                            <input 
+                            type="email" 
+                            className="form-control" 
+                            id='email' name="email" 
+                            value={email} 
+                            onChange={onChange} 
+                            placeholder="Enter your email address" 
+                            required />
+                        </div>
+                        <div className="form-group">
+                            <input 
+                            type="password" 
+                            className="form-control" 
+                            id='password' 
+                            name="password" 
+                            value={password} 
+                            onChange={onChange} 
+                            placeholder="Enter a password" 
+                            required />
+                        </div>
+                        <div className="form-group">
+                            <input 
+                            type="password" 
+                            className="form-control" 
+                            id='password2' 
+                            name="password2" 
+                            value={password2} 
+                            onChange={onChange} 
+                            placeholder="Confirm your password" 
+                            required />
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-block">Submit</button>
+                        </div>
 
-                </form>
-            </section>
-        </>
+                    </form>
+                </div>
+            </div>
+        </section>
     )
 }
 
