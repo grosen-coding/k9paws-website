b09aa4f97b1b82fd8393729952dcc77e2767d9f8
k9paws <candogservices@gmail.com>
2022-04-21 20:39:21 -0400

    about styling only styling remaining before responsiveness

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,14 +10,15 @@ function About() {
 
         <div className="about--content">
 
-          <div className="about--content-left">
+
+    <h2>More info to come...</h2>
+          {/* <div className="about--content-left">
             <h3 className="about--title-sub">left side</h3>
           </div>
 
           <div className="about--content-right">
             <h3 className="about--title-sub">right side</h3>
-            {/* <Slideshow /> */}
-          </div>
+          </div> */}
 
         </div>
 
