74ea6e2c2d3cae4d317a0895af9ca349d78bb810
grosen-coding <rosengsr@gmail.com>
2023-01-24 01:52:09 -0500

    changed link to my portfolio in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,12 @@
-
-
 function Footer() {
   return (
-
-    <footer className="footer">&copy;2016 Designed, Created & Maintained by K<span className='logo--nine-sm'>9</span>PAWS</footer>
-  )
+    <footer className="footer">
+      &copy;2016 Designed, Created & Maintained by &nbsp;{" "}
+      <a href="http://www.gary-rosen.com" target="_blank" rel="noreferrer">
+        <strong> GSRCoding</strong>
+      </a>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
